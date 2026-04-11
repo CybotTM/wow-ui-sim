4e73e2141e4efbeaddeaf5ee291651986da5f59a
Alessio Deiana <adeiana@gmail.com>
2026-04-11 12:54:07 -0500

    Extract IsMouseOver, GetSourceLocation, Intersects from add_region_stub_methods
    Split 70-line function into 4 focused method registrations.
    add_region_stub_methods now 7 body lines. Also compacted Intersects
    to use chained and_then instead of double unwrap.

--- src/lua_api/frame/methods/methods_core_region.rs
+++ src/lua_api/frame/methods/methods_core_region.rs
@@ -33,72 +33,10 @@ fn add_rect_query_methods<M: mlua::UserDataMethods<FrameRef>>(methods: &mut M) {
 
 fn add_region_stub_methods<M: mlua::UserDataMethods<FrameRef>>(methods: &mut M) {
     methods.add_method("IsObjectLoaded", |_lua, _this, ()| Ok(true));
-    methods.add_method("IsMouseOver", |lua, this, args: mlua::MultiValue| {
-        let id = this.0;
-        let (left, right, top, bottom) = parse_mouse_over_offsets(args);
-        let state_rc = get_sim_state(lua);
-        let mut state = state_rc.borrow_mut();
-        state.resolve_rect_if_dirty(id);
-        let Some((mouse_x, mouse_y)) = state.mouse_position else {
-            return Ok(false);
-        };
-        let Some(frame) = state.widgets.get(id) else {
-            return Ok(false);
-        };
-        if !frame.visible || frame.effective_alpha <= 0.0 || !frame.mouse_enabled {
-            return Ok(false);
-        }
-        let Some(rect) = frame.layout_rect else {
-            return Ok(false);
-        };
-
-        let left_edge = rect.x - left;
-        let right_edge = rect.x + rect.width + right;
-        let top_edge = rect.y - top;
-        let bottom_edge = rect.y + rect.height + bottom;
-
-        Ok(mouse_x >= left_edge
-            && mouse_x <= right_edge
-            && mouse_y >= top_edge
-            && mouse_y <= bottom_edge)
-    });
+    add_is_mouse_over_method(methods);
     methods.add_method("StopAnimating", |_lua, _this, ()| Ok(()));
-    methods.add_method("GetSourceLocation", |lua, this, ()| {
-        let state_rc = get_sim_state(lua);
-        let state = state_rc.borrow();
-        let Some(frame) = state.widgets.get(this.0) else {
-            return Ok(Value::Nil);
-        };
-        let Some(location) = source_location_for_owner(&state, frame.owner_addon) else {
-            return Ok(Value::Nil);
-        };
-        Ok(Value::String(lua.create_string(&location)?))
-    });
-    methods.add_method("Intersects", |lua, this, region: Value| {
-        let Some(other_id) = extract_frame_id(&region) else {
-            return Ok(false);
-        };
-        let state_rc = get_sim_state(lua);
-        let mut state = state_rc.borrow_mut();
-        let this_id = this.0;
-        state.resolve_rect_if_dirty(this_id);
-        state.resolve_rect_if_dirty(other_id);
-
-        let Some(this_frame) = state.widgets.get(this_id) else {
-            return Ok(false);
-        };
-        let Some(other_frame) = state.widgets.get(other_id) else {
-            return Ok(false);
-        };
-        let Some(this_rect) = this_frame.layout_rect else {
-            return Ok(false);
-        };
-        let Some(other_rect) = other_frame.layout_rect else {
-            return Ok(false);
-        };
-
-        Ok(layout_rects_intersect(this_rect, other_rect))
-    });
+    add_get_source_location_method(methods);
+    add_intersects_method(methods);
     methods.add_method("IsDrawLayerEnabled", |lua, this, layer: String| {
         let Some(layer) = draw_layer_from_name(&layer) else {
             return Ok(false);
@@ -166,4 +104,66 @@ fn layout_rects_intersect(a: crate::LayoutRect, b: crate::LayoutRect) -> bool {
     let right = (a.x + a.width).min(b.x + b.width);
     let bottom = (a.y + a.height).min(b.y + b.height);
     right > left && bottom > top
+}
+
+fn add_is_mouse_over_method<M: mlua::UserDataMethods<FrameRef>>(methods: &mut M) {
+    methods.add_method("IsMouseOver", |lua, this, args: mlua::MultiValue| {
+        let id = this.0;
+        let (left, right, top, bottom) = parse_mouse_over_offsets(args);
+        let state_rc = get_sim_state(lua);
+        let mut state = state_rc.borrow_mut();
+        state.resolve_rect_if_dirty(id);
+        let Some((mouse_x, mouse_y)) = state.mouse_position else {
+            return Ok(false);
+        };
+        let Some(frame) = state.widgets.get(id) else {
+            return Ok(false);
+        };
+        if !frame.visible || frame.effective_alpha <= 0.0 || !frame.mouse_enabled {
+            return Ok(false);
+        }
+        let Some(rect) = frame.layout_rect else {
+            return Ok(false);
+        };
+        let in_bounds = mouse_x >= rect.x - left
+            && mouse_x <= rect.x + rect.width + right
+            && mouse_y >= rect.y - top
+            && mouse_y <= rect.y + rect.height + bottom;
+        Ok(in_bounds)
+    });
+}
+
+fn add_get_source_location_method<M: mlua::UserDataMethods<FrameRef>>(methods: &mut M) {
+    methods.add_method("GetSourceLocation", |lua, this, ()| {
+        let state_rc = get_sim_state(lua);
+        let state = state_rc.borrow();
+        let Some(frame) = state.widgets.get(this.0) else {
+            return Ok(Value::Nil);
+        };
+        let Some(location) = source_location_for_owner(&state, frame.owner_addon) else {
+            return Ok(Value::Nil);
+        };
+        Ok(Value::String(lua.create_string(&location)?))
+    });
+}
+
+fn add_intersects_method<M: mlua::UserDataMethods<FrameRef>>(methods: &mut M) {
+    methods.add_method("Intersects", |lua, this, region: Value| {
+        let Some(other_id) = extract_frame_id(&region) else {
+            return Ok(false);
+        };
+        let state_rc = get_sim_state(lua);
+        let mut state = state_rc.borrow_mut();
+        let this_id = this.0;
+        state.resolve_rect_if_dirty(this_id);
+        state.resolve_rect_if_dirty(other_id);
+
+        let Some(this_rect) = state.widgets.get(this_id).and_then(|f| f.layout_rect) else {
+            return Ok(false);
+        };
+        let Some(other_rect) = state.widgets.get(other_id).and_then(|f| f.layout_rect) else {
+            return Ok(false);
+        };
+        Ok(layout_rects_intersect(this_rect, other_rect))
+    });
 }
