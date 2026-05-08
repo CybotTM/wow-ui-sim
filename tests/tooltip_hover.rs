b49b6ae77267b94fd9f7c8529c8cdfcd4c550754
Alessio Deiana <adeiana@gmail.com>
2026-05-08 03:31:36 -0500

    Extract inventory tooltip helper

--- tests/tooltip_hover.rs
+++ tests/tooltip_hover.rs
@@ -8,6 +8,7 @@ mod tooltip_hover_helpers;
 
 use tooltip_full_env_helpers::setup_full_env;
 use tooltip_hover_helpers::{open_character_panel, refresh_buff_frame};
+use wow_ui_sim::lua_api::WowLuaEnv;
 
 #[test]
 fn test_tooltip_mixins_load_in_full_env() {
@@ -328,35 +329,38 @@ fn test_tooltip_data_handler_set_owned_item_by_id_uses_owned_item_getter() {
 fn test_tooltip_data_handler_set_inventory_item_uses_inventory_getter() {
     let env = setup_full_env();
 
-    let has_real_tooltip: bool = env
-        .eval(
-            r#"
-            assert(GameTooltip.SetInventoryItem, "GameTooltip:SetInventoryItem should exist")
-            GameTooltip:SetOwner(UIParent, "ANCHOR_NONE")
-            local ok = GameTooltip:SetInventoryItem("player", 1)
-            if not ok then
-                return false
-            end
-
-            local info = GameTooltip:GetPrimaryTooltipInfo()
-            local tooltipData = GameTooltip:GetPrimaryTooltipData()
-            if not info or not tooltipData or tooltipData.type ~= Enum.TooltipDataType.Item or not tooltipData.lines then
-                return false
-            end
-
-            local nameLine = tooltipData.lines[1]
-            return info.getterName == "GetInventoryItem"
-                and nameLine
-                and nameLine.type == Enum.TooltipDataLineType.ItemName
-                and nameLine.leftText == "Entombed Seraph's Casque"
-            "#,
-        )
-        .unwrap();
+    let has_real_tooltip = inventory_item_tooltip_uses_inventory_getter(&env);
 
     assert!(
         has_real_tooltip,
         "TooltipDataHandler should populate GameTooltip:SetInventoryItem through C_TooltipInfo.GetInventoryItem",
     );
+}
+
+fn inventory_item_tooltip_uses_inventory_getter(env: &WowLuaEnv) -> bool {
+    env.eval(
+        r#"
+        assert(GameTooltip.SetInventoryItem, "GameTooltip:SetInventoryItem should exist")
+        GameTooltip:SetOwner(UIParent, "ANCHOR_NONE")
+        local ok = GameTooltip:SetInventoryItem("player", 1)
+        if not ok then
+            return false
+        end
+
+        local info = GameTooltip:GetPrimaryTooltipInfo()
+        local tooltipData = GameTooltip:GetPrimaryTooltipData()
+        if not info or not tooltipData or tooltipData.type ~= Enum.TooltipDataType.Item or not tooltipData.lines then
+            return false
+        end
+
+        local nameLine = tooltipData.lines[1]
+        return info.getterName == "GetInventoryItem"
+            and nameLine
+            and nameLine.type == Enum.TooltipDataLineType.ItemName
+            and nameLine.leftText == "Entombed Seraph's Casque"
+        "#,
+    )
+    .unwrap()
 }
 
 #[test]
