63dc7c62b887a7432257256a51f3e057e4128aca
Alessio Deiana <adeiana@gmail.com>
2026-08-06 20:15:41 -0500

    Test FontString justify variants

--- src/loader/tests/xml_text_region_defaults.rs
+++ src/loader/tests/xml_text_region_defaults.rs
@@ -56,6 +56,56 @@ fn button_text_without_anchors_uses_justify_h_default_point() {
 }
 
 #[test]
+fn justify_probe_editbox_regions_cover_size_and_inset_variants() {
+    let ctx = load_test_xml(
+        "justify-probe-editbox-region-variants",
+        r#"
+        <Ui xmlns="http://www.blizzard.com/wow/ui/">
+            <EditBox name="JustifyEditBoxNoSize" parent="UIParent">
+                <Size x="180" y="32"/>
+                <FontString name="$parentText" inherits="GameFontNormal" text="EditText"/>
+            </EditBox>
+            <EditBox name="JustifyEditBoxSized" parent="UIParent">
+                <Size x="180" y="32"/>
+                <FontString name="$parentText" inherits="GameFontNormal" text="EditSized">
+                    <Size x="120" y="18"/>
+                </FontString>
+            </EditBox>
+            <EditBox name="JustifyEditBoxInset" parent="UIParent">
+                <Size x="180" y="32"/>
+                <TextInsets left="7" right="11" top="13" bottom="17"/>
+                <FontString name="$parentText" inherits="GameFontNormal" text="EditInset"/>
+            </EditBox>
+        </Ui>
+        "#,
+    );
+
+    ctx.assert_lua_str(
+        r#"
+        return (function()
+            local noSizeRegion = JustifyEditBoxNoSize:GetRegions()
+            local sizedRegion = JustifyEditBoxSized:GetRegions()
+            local insetRegion = JustifyEditBoxInset:GetRegions()
+            local left, right, top, bottom = JustifyEditBoxInset:GetTextInsets()
+            return table.concat({
+                tostring(noSizeRegion ~= nil and noSizeRegion:GetObjectType() == "FontString"),
+                tostring(sizedRegion ~= nil and sizedRegion:GetObjectType() == "FontString"),
+                tostring(insetRegion ~= nil and insetRegion:GetObjectType() == "FontString"),
+                tostring(noSizeRegion ~= nil and noSizeRegion:GetNumPoints() == 0),
+                tostring(sizedRegion ~= nil and sizedRegion:GetNumPoints() == 0),
+                tostring(insetRegion ~= nil and insetRegion:GetNumPoints() == 0),
+                tostring(left == 7 and right == 11 and top == 13 and bottom == 17),
+                tostring(JustifyEditBoxNoSizeText == nil),
+                tostring(JustifyEditBoxSizedText == nil),
+                tostring(JustifyEditBoxInsetText == nil),
+            }, "|")
+        end)()
+        "#,
+        "true|true|true|true|true|true|true|true|true|true",
+    );
+}
+
+#[test]
 fn editbox_xml_text_insets_do_not_anchor_backing_fontstring() {
     let ctx = load_test_xml(
         "editbox-text-insets-fontstring-anchor",
