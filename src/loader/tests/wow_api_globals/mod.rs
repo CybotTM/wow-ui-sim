6f44ef24d370a7b292a389b6e2dc634d56746671
Alessio Deiana <adeiana@gmail.com>
2026-08-10 14:49:22 -0500

    Test enemy-NPC nameplate aura enums

--- src/loader/tests/wow_api_globals/mod.rs
+++ src/loader/tests/wow_api_globals/mod.rs
@@ -71,6 +71,7 @@ mod patch_12_0_0_encounter_timeline_track_enums;
 mod patch_12_0_0_expansion_landing_page_type_removals;
 mod patch_12_0_0_item_creation_context_removals;
 mod patch_12_0_0_nameplate_cast_bar_display_enums;
+mod patch_12_0_0_nameplate_enemy_npc_aura_display_enums;
 mod patch_12_0_0_small_enums;
 mod patch_12_0_0_ui_enum_metadata;
 mod patch_12_0_0_unit_power_spell_ids;
