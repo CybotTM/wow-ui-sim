b7bf42409210b4da7658d605aba3848f63dd8b0d
Alessio Deiana <adeiana@gmail.com>
2026-08-10 22:15:42 -0500

    Test transmog outfit slot position enum

--- src/loader/tests/wow_api_globals/mod.rs
+++ src/loader/tests/wow_api_globals/mod.rs
@@ -95,6 +95,7 @@ mod patch_12_0_0_transmog_outfit_slot_flags_enums;
 mod patch_12_0_0_transmog_outfit_slot_metadata_enums;
 mod patch_12_0_0_transmog_outfit_slot_option_flags_enums;
 mod patch_12_0_0_transmog_outfit_slot_option_metadata_enums;
+mod patch_12_0_0_transmog_outfit_slot_position_enums;
 mod patch_12_0_0_ui_enum_metadata;
 mod patch_12_0_0_unit_power_spell_ids;
 mod patch_12_1_service_payloads;
