a1e8065e7747d16f4dbdb023539ef8c347f3b70e
Alessio Deiana <adeiana@gmail.com>
2026-08-11 05:57:02 -0500

    Test raid dispel display type

--- src/loader/tests/wow_api_globals/mod.rs
+++ src/loader/tests/wow_api_globals/mod.rs
@@ -107,6 +107,7 @@ mod patch_12_0_0_prey_hunt_progress_state_enums;
 mod patch_12_0_0_procedural_spawn_interaction_mode_enums;
 mod patch_12_0_0_procedural_spawn_volume_chunk_flags_enums;
 mod patch_12_0_0_raid_aura_organization_type_enums;
+mod patch_12_0_0_raid_dispel_display_type_enums;
 mod patch_12_0_0_small_enums;
 mod patch_12_0_0_status_bar_timer_direction_enums;
 mod patch_12_0_0_tooltip_data_line_type_enums;
