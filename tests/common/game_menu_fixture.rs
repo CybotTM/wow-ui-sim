c31a5ce12da743295f95730b35a6011801e54cda
Alessio Deiana <adeiana@gmail.com>
2026-08-26 15:40:27 -0500

    Load real HelpFrame in GameMenu fixture

--- tests/common/game_menu_fixture.rs
+++ tests/common/game_menu_fixture.rs
@@ -79,7 +79,9 @@ const BLIZZARD_ADDONS: &[(&str, &str)] = &[
     ),
     ("Blizzard_WorldMap", "Blizzard_WorldMap_Mainline.toc"),
     ("Blizzard_ActionBar", "Blizzard_ActionBar_Mainline.toc"),
+    ("Blizzard_StaticPopup_Game", "Blizzard_StaticPopup_Game.toc"),
     ("Blizzard_GameMenu", "Blizzard_GameMenu_Mainline.toc"),
+    ("Blizzard_HelpFrame", "Blizzard_HelpFrame.toc"),
 ];
 
 pub fn setup_game_menu_env() -> WowLuaEnv {
